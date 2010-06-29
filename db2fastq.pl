6191b8af7c44eb2a14013c315b1b40045f54dae3
Christiaan Klijn <c.klijn@gmail.com>
2010-06-29 14:35:41 +0000

    Started work on a way to export FASTQs from the SQLite databases created during mapping
    git-svn-id: http://bioinformatics.nki.nl/svn/IHD-R-BioC-packages/trunk/shearing/pipeline@369 e9328c4f-6670-0410-aa1d-bbebe5fdda6e

---
+++ db2fastq.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+#===============================================================================
+#
+#         FILE:  db2fastq.pl
+#
+#        USAGE:  ./db2fastq.pl  
+#
+#  DESCRIPTION:  
+#
+#      OPTIONS:  ---
+# REQUIREMENTS:  ---
+#         BUGS:  ---
+#        NOTES:  ---
+#       AUTHOR:  C Klijn, c.klijn@nki.nl
+#      COMPANY:  NKI
+#      VERSION:  0.1
+#      CREATED:  06/29/2010
+#     REVISION:  ---
+#===============================================================================
+
+
+
