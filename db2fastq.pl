22a2c001b4c1ac97d05e6088a73e00e83d0d3d0c
Christiaan Klijn <c.klijn@gmail.com>
2010-06-29 15:34:15 +0000

    finished basic FASTA output perl script from one of the SQLite databases
    git-svn-id: http://bioinformatics.nki.nl/svn/IHD-R-BioC-packages/trunk/shearing/pipeline@370 e9328c4f-6670-0410-aa1d-bbebe5fdda6e

--- db2fastq.pl
+++ db2fastq.pl
@@ -18,5 +18,26 @@
 #     REVISION:  ---
 #===============================================================================
 
+use strict;
+use warnings;
+use DBI;
 
+# Run parameters
+# 1 - database name (default insert.db)
+
+my $dbf = shift || "insert.db";
+
+# Connect to database
+
+my $dbh = DBI->connect("dbi:SQLite:dbname=$dbf","","", { RaiseError => 1, AutoCommit => 0});
+
+# Design and run query
+my $q1 = "select * from read";
+my $h1 = $dbh->prepare($q1);
+$h1->execute();
+
+while (my @row = $h1->fetchrow_array) {
+	my ($readID, $runbc, $seq) = @row[1,2,4];
+	print ">", join(":", $readID, $runbc) , "\n", $seq, "\n";
+}
 
