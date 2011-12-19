4e6230503ac6d2ff069c00106bccaa15518b11de
Christiaan Klijn <c.klijn@gmail.com>
2011-12-19 16:43:50 +0100

    Adjustments to bash scripts to run on /data/medoid-groups/im-mapping-pipeline/

--- pipeline_MULV.sh
+++ pipeline_MULV.sh
@@ -7,13 +7,19 @@
 FNA=$1
 BASE=$2
 MKDB=$3
+BCFILE=$4
 
-blat ~/mouseref/mouse_ref.fa $FNA -out=psl -minScore=10 $BASE.psl 2> $BASE.blatlog
-exonerate -s 75 $FNA ~/shearing/vecfiles/vec.fa > $BASE.exo &
-exonerate -s 49 --exhaustive yes $FNA ~/shearing/vecfiles/MULVbc.fa > $BASE.bc.exo &
-~/shearing/pipeline/process_MULV.pl $FNA $BASE $BASE $BASE $MKDB > $BASE.tsv 2> $BASE.log
-~/shearing/pipeline/makeRef.pl $BASE
+# Location of the pipeline and the reference files
+# Change when applicable to a local install
+reffile=/data/medoid-groups/im-mapping-pipeline/resource/mouse_ref.fa
+pipelinedir=/data/medoid-groups/im-mapping-pipeline/NKIpipelineIM
+
+blat $reffile $FNA -out=psl -minScore=10 $BASE.psl 2> $BASE.blatlog
+exonerate -s 75 $FNA $pipelinedir/vecfiles/vec.fa > $BASE.exo &
+exonerate -s 25 $FNA $BCFILE > $BASE.bc.exo &
+$pipelinedir/process_MULV.pl $FNA $BASE $BASE $BASE $MKDB > $BASE.tsv 2> $BASE.log
+$pipelinedir/makeRef.pl $BASE
 blat $BASE.ref.fa $BASE.que.fa -minScore=10 -out=psl $BASE.2.psl
-~/shearing/pipeline/assign.pl $BASE.2.psl 200 $BASE >> $BASE.log
-~/shearing/pipeline/export_nogenes.pl $BASE MULV > $BASE.export.txt
-#~/shearing/pipeline/export.pl $BASE insert.db 1 > $BASE.export_squated.txt
+$pipelinedir/assign.pl $BASE.2.psl 200 $BASE >> $BASE.log
+$pipelinedir/export_nogenes.pl $BASE > $BASE.export.txt
+# $pipelinedir/export.pl $BASE insert.db 1 > $BASE.export_squated.txt
--- pipeline_PB.sh
+++ pipeline_PB.sh
@@ -7,13 +7,19 @@
 FNA=$1
 BASE=$2
 MKDB=$3
+BCFILE=$4
 
-# blat ~/mouseref/mouse_ref.fa $FNA -out=psl -minScore=10 $BASE.psl 2> $BASE.blatlog
-# exonerate -s 75 $FNA ~/marco/vecfiles/vec.fa > $BASE.exo &
-# exonerate -s 25 $FNA ~/marco/vecfiles/dummy.fa > $BASE.bc.exo &
-# ~/shearing/pipeline/process_PB.pl $FNA $BASE $BASE $BASE $MKDB > $BASE.tsv 2> $BASE.log
-# ~/shearing/pipeline/makeRef.pl $BASE
-# blat $BASE.ref.fa $BASE.que.fa -minScore=10 -out=psl $BASE.2.psl
-# ~/shearing/pipeline/assign.pl $BASE.2.psl 200 $BASE >> $BASE.log
-~/shearing/pipeline/export_nogenes.pl $BASE TRNSP5LTR insert.db 0 10000 50 SPLINK > $BASE.export.txt 2> $BASE.exp.log
-#~/marco/pipeline/export.pl $BASE insert.db 1 > $BASE.export_squated.txt
+# Location of the pipeline and the reference files
+# Change when applicable to a local install
+reffile=/data/medoid-groups/im-mapping-pipeline/resource/mouse_ref.fa
+pipelinedir=/data/medoid-groups/im-mapping-pipeline/NKIpipelineIM
+
+blat $reffile $FNA -out=psl -minScore=10 $BASE.psl 2> $BASE.blatlog
+exonerate -s 75 $FNA $pipelinedir/vecfiles/vec.fa > $BASE.exo &
+exonerate -s 25 $FNA $BCFILE > $BASE.bc.exo &
+$pipelinedir/process_PB.pl $FNA $BASE $BASE $BASE $MKDB > $BASE.tsv 2> $BASE.log
+$pipelinedir/makeRef.pl $BASE
+blat $BASE.ref.fa $BASE.que.fa -minScore=10 -out=psl $BASE.2.psl
+$pipelinedir/assign.pl $BASE.2.psl 200 $BASE >> $BASE.log
+$pipelinedir/export_nogenes.pl $BASE > $BASE.export.txt
+# $pipelinedir/export.pl $BASE insert.db 1 > $BASE.export_squated.txt
